5f1478137a4fcec19d17f47b4eb70c64d8b62db7
Silva-Sebastian <sebastian.gaboto@hotmail.com>
2022-10-25 14:49:50 -0300

    usando js

---
+++ js/algoritmo.js
@@ -0,0 +1,18 @@
+let compra = prompt("vas a comprar algun termo de acero??")
+
+if (compra == "si") {
+    alert ("entonces el envio es gratis")
+} else {
+    alert ("habra que calcular en elvio")
+}
+
+/*let edad = parseInt(prompt("edad"))
+
+let cond = edad == 18;
+console.log(cond);
+
+if (cond) {
+    alert("tiene 18")
+} else {
+    alert("no tiene")
+}*/

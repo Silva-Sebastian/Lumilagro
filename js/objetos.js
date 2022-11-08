dd2dbae52100aeda2f94681a3c3c904c0bcdd874
Silva Sebastian <sebastian.gaboto@hotmail.com>
2022-11-08 20:20:11 -0300

    completando entrega 2

--- js/objetos.js
+++ js/objetos.js
@@ -6,10 +6,23 @@ class Termo{
     }
 }
 
-const Termo1 = new Termo ("Luminox", "Negro", "$12000")
-const Termo2 = new Termo ("Tango", "Fucsia", "$6000")
-const Termo3 = new Termo ("Isotermico", "Azul", "$4000")
+const Termo1 = new Termo ("Luminox", "Acero", "$12000")
+const Termo2 = new Termo ("Tango", "Vidrio", "$6000")
+const Termo3 = new Termo ("Isotermico", "Plastico", "$4000")
 
-console.log(Termo1)
-console.log(Termo2)
-console.log(Termo3)
+ListaTermos = [Termo1, Termo2, Termo3]
+console.log(ListaTermos[1])
+
+
+
+const Acero = [
+    {color: "Negro", precio: 13000},
+    {color: "Blanco", precio: 14000},
+    {color: "Seleccion", precio: 16000}
+]
+Acero.push({color: "Bordo", precio: 13000});
+const res = Acero.filter((el) => el.precio <= 14000)
+const res1 = Acero.filter((el) => el.color.includes('B'))
+
+console.log(res)
+console.log(res1)
